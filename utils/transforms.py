a35fbcdd90b3ec76ec8ada2d585e7c3fa690e47c
Tyler Howell <tyler.howell1202@gmail.com>
2025-11-15 14:03:31 -0600

    modified transforms to 1D, added training and eval notebook example

--- utils/transforms.py
+++ utils/transforms.py
@@ -2,18 +2,20 @@ from torchvision import transforms
 
 def get_train_transforms():
     return transforms.Compose([
+        transforms.Grayscale(num_output_channels=1), #ensure remains grayscale (1 output channel)
         transforms.Resize((224, 224)),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(degrees=10),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],  #ImageNet stats for now
-                             std=[0.229, 0.224, 0.225])
+        transforms.Normalize(mean=[0.485],
+                             std=[0.229])
     ])
 
 def get_val_transforms():
     return transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
+        transforms.Normalize(mean=[0.485],
+                             std=[0.229])
     ])
